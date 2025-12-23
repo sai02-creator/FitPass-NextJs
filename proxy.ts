da63f9658f80d47c057733397d2c563dc0068cde
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-12-24 02:23:47 +1100

    Initial commit: FitPass Next.js project

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,14 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
+
+const isProtectedRoute = createRouteMatcher([
+  "/classes(.*)",
+  "/bookings(.*)",
+  "/profile(.*)",
+  "/upgrade(.*)",
+  "/onboarding(.*)",
+]);
+
 export default clerkMiddleware();
 
 export const config = {
