a194528c57188029bb0830dac4e16a5e074d8651
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-12-25 00:56:01 +1100

    feat(auth): wrap app with ClerkProvider

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,10 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import React from 'react'
+
+const AppLayout = ({children}: {children: React.ReactNode}) => {
+  return (
+    <ClerkProvider>{children}</ClerkProvider>
+  )
+}
+
+export default AppLayout
