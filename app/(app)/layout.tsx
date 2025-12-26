03e29a2cc48461408b67cc33fed980670fb143c0
sai02-creator <saichaitanyagaddam1@gmail.com>
2025-12-27 01:23:51 +1100

    import OnboardingGuard for protecting routes and redirecting users who haven't completed onboarding

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,14 +1,16 @@
+import { AppHeader } from '@/components/app/layout/AppHeader'
+import { OnboardingGuard } from '@/components/app/onboarding/OnboardingGuard'
 import { SanityLive } from '@/sanity/lib/live'
 import { ClerkProvider } from '@clerk/nextjs'
 
-const AppLayout = ({children}: {children: React.ReactNode}) => {
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      
-      {children}
-    
-
-    <SanityLive />
+      <OnboardingGuard>
+        <AppHeader />
+        {children}
+      </OnboardingGuard>
+      <SanityLive />
     </ClerkProvider>
   )
 }
